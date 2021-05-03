ec6b780cbfae44e3412ef8fa94655af97a7f2dab
faridgibrandrei <faridgibrandrei@gmail.com>
2021-05-03 20:39:16 +0700

    Update : - Added SignIn Page - Added Logo & Emblem assets - Update Logo on Splash & GetStarted Page

--- app/src/main/java/com/drop/pariwisata/GetStartedAct.java
+++ app/src/main/java/com/drop/pariwisata/GetStartedAct.java
@@ -1,14 +1,26 @@
 package com.drop.pariwisata;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GetStartedAct extends AppCompatActivity {
 
+    Button btnSignIn, btnNewAccount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
+
+        btnSignIn = findViewById(R.id.btn_sign_in);
+        btnNewAccount = findViewById(R.id.btn_new_account);
+
+        btnSignIn.setOnClickListener(v -> {
+            Intent goToSignIn = new Intent(GetStartedAct.this, SignInAct.class);
+            startActivity(goToSignIn);
+        });
     }
 }
