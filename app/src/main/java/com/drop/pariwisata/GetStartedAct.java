9f4d1ba3ca07d2a34ac147c33dc286731befae14
faridgibrandrei <faridgibrandrei@gmail.com>
2021-05-03 13:08:31 +0700

    Update : - Added GetStarted Page - Added Button Styles

---
+++ app/src/main/java/com/drop/pariwisata/GetStartedAct.java
@@ -0,0 +1,14 @@
+package com.drop.pariwisata;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GetStartedAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_started);
+    }
+}
